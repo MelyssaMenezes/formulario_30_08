07745ab2e73f3b3635f8a07eea858e9b556b4401
MelyssaMenezes <melyssa.smenezes@gmail.com>
2023-08-31 12:27:27 -0300

    criado a funcao do botao para salvar novos registros

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
 console.log("Pagina carregada!!");
 
+ // Lista de clientes (array de objeto)
+let customerList = [];
+ /*o let permite que declare variaveis no escopo em bloco,
+ instrucao ou uma expressao na qual ela é usada */
+ 
+//document.querySelector - vai procurar no documento algum ID fieldName
+const edtName = document.querySelector('#fieldName');
+const edtAddress = document.querySelector('#fieldAddress');
+const edtAge = document.querySelector('#fieldAge');
 
-/* addEventListener - permite q configure funcoes pra ser chamadas
+
+ /* addEventListener - permite q configure funcoes pra ser chamadas
  quando um evento acontece, exemplo: submit */
 document.addEventListener('submit', function(event){
     event.preventDefault();
-/*O método event.preventDefault() evita o comportamento padrão de um
- elemento. Evita da pagina ser recarregada qundo o formulario for
-submetido*/
+    /*O método event.preventDefault() evita o comportamento padrão de um
+    elemento. Evita da pagina ser recarregada qundo o formulario for
+    submetido*/
 
     console.log("Formulario submetido"); 
     //ira aparecer quando o botao salvar for clicado
+
+    saveCustomer();
+
 });
 
+function saveCustomer(){
+    console.log('Funcao saveCustomer executado!');
+}
 
-/*atribuir na variavel edtName o valor q foi digitado onde tem o ID 
- fieldName */
-const edtName = document.getElementById('fieldName');
-// getElementById - retorna o elemento cujo o ID foi especificado
-
-const nameValue = edtName.value;
-// guarda o valor digitado no edtName no nameValue
-
-console.log('Nome:', nameValue);
+// pegando os edits(campos) digitados
+const edtNameValue = edtName.value;
+const edtAddressValue = edtAddress.value;
+const edtAgeValue = edtAge.value;
 
 
-const edtAddress = document.getElementById('fieldAddress');
-const addressValue = edtAddress.value;
-console.log('Endereco:', addressValue);
+console.log(edtNameValue, edtAddressValue, edtAgeValue);
 
+// customer - cliente
+const customer = {
+    name: edtNameValue,
+    address: edtAddressValue,
+    age: edtAge
+}
 
-const edtAge = document.getElementById('fieldAge');
-const ageValue = edtAge.value;
-console.log('Idade:', ageValue);
+/*Adiciona(push) objeto (customer) na lista de clientes
+(array customerList)*/
+customerList.push(customer);
+
+console.log(customerList);
