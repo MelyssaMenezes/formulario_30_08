86c59eae9a400f8074bb5e8e4416f9bcc866b931
MelyssaMenezes <melyssa.smenezes@gmail.com>
2023-08-31 15:23:07 -0300

    criado a funcao para limpar o formulario e foco no campo nome

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 console.log("Pagina carregada!!");
 
- // Lista de clientes (array de objeto)
+// Lista de clientes (array de objeto)
 let customerList = [];
  /*o let permite que declare variaveis no escopo em bloco,
  instrucao ou uma expressao na qual ela é usada */
  
+
+//main form - formulario principal
+const mainForm = document.querySelector('#mainForm');
+/* vai procurar no html um ID mainForm q ta na tag form e guardar
+na variavel mainForm*/
+
+
 //document.querySelector - vai procurar no documento algum ID fieldName
 const edtName = document.querySelector('#fieldName');
 const edtAddress = document.querySelector('#fieldAddress');
 const edtAge = document.querySelector('#fieldAge');
 
+//chamando a funcao clearForm
+clearForm();
 
  /* addEventListener - permite q configure funcoes pra ser chamadas
  quando um evento acontece, exemplo: submit */
@@ -22,7 +31,11 @@ document.addEventListener('submit', function(event){
     console.log("Formulario submetido"); 
     //ira aparecer quando o botao salvar for clicado
 
+    // chamando a funcao para salvar os dados dos clientes
     saveCustomer();
+    /* chamando a funcao para limpar o formulario e por o foco no 
+     campo do nome */
+    clearForm();
 
 });
 
@@ -51,6 +64,14 @@ function saveCustomer(){
 
     console.log(customerList);
 
+}
+
+//criando uma funcao para limpar o formulario
+function clearForm() {
+    mainForm.reset(); // reset vai limpar o formulario
+    edtName.focus(); 
+    /* focus vai deixar o cursor no campo nome depois de recarregar
+     a pagina ou salvar os dados */
 }
 
 
