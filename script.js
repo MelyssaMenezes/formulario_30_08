5ca6676ab7e8c0b5f89489b9af33f708862aec8d
MelyssaMenezes <melyssa.smenezes@gmail.com>
2023-08-31 11:05:42 -0300

    criada a funcao dos campos do formulario

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ submetido*/
     //ira aparecer quando o botao salvar for clicado
 });
 
+
+/*atribuir na variavel edtName o valor q foi digitado onde tem o ID 
+ fieldName */
+const edtName = document.getElementById('fieldName');
+// getElementById - retorna o elemento cujo o ID foi especificado
+
+const nameValue = edtName.value;
+// guarda o valor digitado no edtName no nameValue
+
+console.log('Nome:', nameValue);
+
+
+const edtAddress = document.getElementById('fieldAddress');
+const addressValue = edtAddress.value;
+console.log('Endereco:', addressValue);
+
+
+const edtAge = document.getElementById('fieldAge');
+const ageValue = edtAge.value;
+console.log('Idade:', ageValue);
